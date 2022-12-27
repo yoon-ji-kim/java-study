f9ba7ac71e09de1b260a28f48a71b67d564b03a5
yoon-ji-kim <yoonjigim0.0@gmail.com>
2022-12-27 16:21:23 +0900

    edited finaltest prob04

--- finaltest/src/main/java/prob04/Person.java
+++ finaltest/src/main/java/prob04/Person.java
@@ -6,6 +6,10 @@ public class Person {
 	private String name;
 	
 	/* 코드 작성 */
+	public Person() {
+		age = 12;
+		name = "";
+	}
 	public Person(String name) {
 		this.age = 12;
 		this.name = name;
